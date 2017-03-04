0979755beaceead29c4551ac5ee25300febdcf7c
Danny Smith <dannyleesmith@outlook.com>
2017-03-04 13:46:01 +0000

    Basic framework complete

--- classes.py
+++ classes.py
@@ -9,10 +9,23 @@ class Scene(object):
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        current_scene.enter()
+
+class Opening(Scene):
+
+    def enter(self):
+        print "Opening."
+        return 'finished'
 
 class Outside(Scene):
 
@@ -74,13 +87,49 @@ class Bedroom4(Scene):
     def enter(self):
         pass
 
+class Bathroom(Scene):
+
+    def enter(self):
+        pass
+
+class Loft(Scene):
+
+    def enter(self):
+        pass
+
+class Finished(Scene):
+
+    def enter(self):
+        print "Finished."
+        exit(0)
+
 class Map(object):
 
+    scenes = {
+        'opening': Opening(),
+        'outside': Outside(),
+        'porch': Porch(),
+        'hallway': Hallway(),
+        'reception': Reception(),
+        'lounge': Lounge(),
+        'kitchen': Kitchen(),
+        'dining_room': DiningRoom(),
+        'landing': Landing(),
+        'bedroom_1': Bedroom1(),
+        'bedroom_2': Bedroom2(),
+        'bedroom_3': Bedroom3(),
+        'bedroom_4': Bedroom4(),
+        'bathroom': Bathroom(),
+        'loft': Loft(),
+        'finished': Finished(),
+    }
+
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        pass
+        val = Map.scenes.get(scene_name)
+        return val
 
-    def opening_scene(self, scene_name):
-        pass
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
--- story.py
+++ story.py
@@ -1,5 +1,5 @@
 from classes import *
 
-a_map = Map('outside')
+a_map = Map('opening')
 a_story = Engine(a_map)
 a_story.play()
